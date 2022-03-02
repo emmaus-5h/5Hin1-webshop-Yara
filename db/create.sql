e5bdb95fe30a935e98be3fc213f27805dd098593
MounirBe <74650457+MounirBe@users.noreply.github.com>
2022-03-02 16:45:18 +0000

    rating en kleuren toegevoegd bij tabel en in de api gezet

--- db/create.sql
+++ db/create.sql
@@ -9,7 +9,9 @@ CREATE TABLE products (
   description TEXT,
   price NUMERIC(10, 2),
   model VARCHAR(255),
-  productiejaar_id INTEGER
+  productiejaar_id INTEGER,
+  kleuren_id INTEGER,
+  rating_id INTEGER
   );
 
   CREATE TABLE productiejaren (
@@ -22,7 +24,10 @@ CREATE TABLE products (
   name VARCHAR(255)
   );
   
- 
+  CREATE TABLE rating (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(255)
+  );
 
 --
 -- populate with data
@@ -33,12 +38,12 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price, model, productiejaar_id) values ('Marmer Terquoise', 'Dit hoesje is heel populair onder jongeren. Het was de best verkochte hoesje van 2021.', '816905633-0', 10.5, 'iPhone 11 ' , 3);
-insert into products (name, description, code, price, model, productiejaar_id) values ('Beige Bees', 'Vrolijk hoesje met beige achtergrond en bijen.', '077030122-3', 11, 'iPhone 11 ' , 1);
-insert into products (name, description, code, price, model, productiejaar_id) values ('Lightning Bolt', 'Een zwarte hoesje met een bliksemschicht in het midden.', '445924201-X', 13.5, 'iPhone 11 ' , 2);
-insert into products (name, description, code, price, model, productiejaar_id) values ('Aqua Sea Blue', 'Met dit hoesje kijk je door je ogen door de zee', '693155505-7', 13.5, 'iPhone 11 ' , 3);
-insert into products (name, description, code, price, model, productiejaar_id) values ('Happy Autumn', 'De vormen geven een warme gevoel en de kleuren een herfstgevoel', '686928463-6', 14, 'iPhone 11 ' , 2);
-insert into products (name, description, code, price, model, productiejaar_id) values ('Special Honeycomb', 'Een coole gebroken zwarte hoesje om jouw onhandige kant te laten zien.', '492662523-7', 14, 'iPhone 11 ' , 1);
+insert into products (name, description, code, price, model, productiejaar_id, kleuren_id, rating_id) values ('Marmer Terquoise', 'Dit hoesje is heel populair onder jongeren. Het was de best verkochte hoesje van 2021.', '816905633-0', 10.5, 'iPhone 11 ' , 3, 'terquoise', '⭐⭐');
+insert into products (name, description, code, price, model, productiejaar_id, kleuren_id, rating_id) values ('Beige Bees', 'Vrolijk hoesje met beige achtergrond en bijen.', '077030122-3', 11, 'iPhone 11 ' , 1, 'geel', '⭐⭐⭐');
+insert into products (name, description, code, price, model, productiejaar_id, kleuren_id, rating_id) values ('Lightning Bolt', 'Een zwarte hoesje met een bliksemschicht in het midden.', '445924201-X', 13.5, 'iPhone 11 ' , 2, 'zwart', '⭐⭐⭐');
+insert into products (name, description, code, price, model, productiejaar_id, kleuren_id, rating_id) values ('Aqua Sea Blue', 'Met dit hoesje kijk je door je ogen door de zee', '693155505-7', 13.5, 'iPhone 11 ' , 3, 'blauw', '⭐');
+insert into products (name, description, code, price, model, productiejaar_id, kleuren_id, rating_id) values ('Happy Autumn', 'De vormen geven een warme gevoel en de kleuren een herfstgevoel', '686928463-6', 14, 'iPhone 11 ' , 2, 'overige', '⭐⭐⭐');
+insert into products (name, description, code, price, model, productiejaar_id, kleuren_id, rating_id) values ('Gebroken Glass', 'Een coole gebroken zwarte hoesje om jouw onhandige kant te laten zien.', '492662523-7', 14, 'iPhone 11 ' , 1, 'zwart', '⭐⭐⭐');
 
 insert into productiejaren (name) values ('2020');
 insert into productiejaren (name) values ('2021'); 
@@ -49,3 +54,8 @@ insert into kleuren (name) values ('beige');
 insert into kleuren (name) values ('blauw');
 insert into kleuren (name) values ('geel');
 insert into kleuren (name) values ('terquoise');
+insert into kleuren (name) values ('zwart');
+
+insert into rating (name) values ('⭐⭐⭐')
+insert into rating (name) values ('⭐⭐')
+insert into rating (name) values ('⭐')
