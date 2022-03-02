44df61733027d64e053ccc965e1945f466313b42
MounirBe <74650457+MounirBe@users.noreply.github.com>
2022-03-02 00:30:11 +0000

    een paar namen verandert en at netter gemaakt

--- db/create.sql
+++ db/create.sql
@@ -15,13 +15,11 @@ CREATE TABLE products (
   CREATE TABLE productiejaren (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(255)
-
   );
 
   CREATE TABLE kleuren (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(255)
-
   );
   
  
@@ -37,10 +35,10 @@ CREATE TABLE products (
 
 insert into products (name, description, code, price, model, productiejaar_id) values ('Marmer Terquoise', 'Dit hoesje is heel populair onder jongeren. Het was de best verkochte hoesje van 2021.', '816905633-0', 10.5, 'iPhone 11 ' , 3);
 insert into products (name, description, code, price, model, productiejaar_id) values ('Beige Bees', 'Vrolijk hoesje met beige achtergrond en bijen.', '077030122-3', 11, 'iPhone 11 ' , 1);
-insert into products (name, description, code, price, model, productiejaar_id) values ('Broken Black', 'Zwart is normaal basic, broken Black hoesje niet.', '445924201-X', 13.5, 'iPhone 11 ' , 2);
+insert into products (name, description, code, price, model, productiejaar_id) values ('Lightning Bolt', 'Een zwarte hoesje met een bliksemschicht in het midden.', '445924201-X', 13.5, 'iPhone 11 ' , 2);
 insert into products (name, description, code, price, model, productiejaar_id) values ('Aqua Sea Blue', 'Met dit hoesje kijk je door je ogen door de zee', '693155505-7', 13.5, 'iPhone 11 ' , 3);
 insert into products (name, description, code, price, model, productiejaar_id) values ('Happy Autumn', 'De vormen geven een warme gevoel en de kleuren een herfstgevoel', '686928463-6', 14, 'iPhone 11 ' , 2);
-insert into products (name, description, code, price, model, productiejaar_id) values ('Special Honeycomb', 'Speciale honingraat met speciale kleuren.', '492662523-7', 14, 'iPhone 11 ' , 1);
+insert into products (name, description, code, price, model, productiejaar_id) values ('Special Honeycomb', 'Een coole gebroken zwarte hoesje om jouw onhandige kant te laten zien.', '492662523-7', 14, 'iPhone 11 ' , 1);
 
 insert into productiejaren (name) values ('2020');
 insert into productiejaren (name) values ('2021'); 
