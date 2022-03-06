a19f293f6af72eb307e7085ed2ac78675ea10255
Yara Abdulqadir <74650455+yaraabdulqadir@users.noreply.github.com>
2022-03-06 22:55:00 +0000

    laatste keer even voor de zekerheid pushen naar github

--- db/create.sql
+++ db/create.sql
@@ -81,7 +81,7 @@ insert into rating (name) values ('⭐');
 
 SELECT productiejaren.id, productiejaren.name FROM productiejaren
   JOIN products
-ON productiejaren.id = products.productiejaar_id
+ON productiejaren.id = product.productiejaar_id
 
   /* aangezien de console bij 2 van bij elkaargevoegde tabellen error geeft, zet ik hem als commentaar neer*/
 
